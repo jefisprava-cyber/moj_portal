679551a5ad0d973ba6c072faae883148193bb10d
jefisprava-cyber <jefisprava@gmail.com>
2026-01-25 15:30:01 +0100

    Oprava urls a register sablony

--- core/urls.py
+++ core/urls.py
@@ -1,17 +1,21 @@
 from django.contrib import admin
 from django.urls import path, include
 from products import views
-from django.contrib.auth import views as auth_views # Pridaj tento import
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('admin/', admin.site.core),
+    # POZOR: Tu musí byť admin.site.urls
+    path('admin/', admin.site.urls), 
+    
     path('', views.home, name='home'),
     path('add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
     path('cart/', views.cart_detail, name='cart_detail'),
     path('remove/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
     path('optimize/', views.optimize_cart, name='optimize_cart'),
+    
+    # Registrácia
     path('register/', views.register, name='register'),
     
-    # TENTO RIADOK CHÝBAL:
+    # Autentifikácia (login, logout atď.)
     path('accounts/', include('django.contrib.auth.urls')), 
 ]
