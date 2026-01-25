05afbda1986dfcc5dd74b5511a4461dd8fbfc56a
jefisprava-cyber <jefisprava@gmail.com>
2026-01-25 20:45:31 +0100

    Feat: Pridane migracie pre objednavky

--- products/models.py
+++ products/models.py
@@ -25,3 +25,33 @@ class CartItem(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.product.name}"
+
+        # products/models.py
+
+class Order(models.Model):
+    # Kto objednáva (môže byť aj neprihlásený, preto user je optional)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    
+    # Kontaktné údaje
+    full_name = models.CharField(max_length=100, verbose_name="Celé meno")
+    email = models.EmailField(verbose_name="Email")
+    address = models.CharField(max_length=200, verbose_name="Adresa")
+    city = models.CharField(max_length=100, verbose_name="Mesto")
+    zip_code = models.CharField(max_length=10, verbose_name="PSČ")
+    
+    # Info o objednávke
+    created_at = models.DateTimeField(auto_now_add=True)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    paid = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Objednávka {self.id} - {self.full_name}"
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2) # Cena v čase nákupu
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity}x {self.product.name}"
