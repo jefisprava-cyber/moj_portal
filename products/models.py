4b51552d88444d5edeb23129803ab3e67318cc7a
jefisprava-cyber <jefisprava@gmail.com>
2026-01-25 17:34:24 +0100

    Update model: Pridane image_url a delivery

--- products/models.py
+++ products/models.py
@@ -5,6 +5,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     shop_name = models.CharField(max_length=100)
+    image_url = models.URLField(max_length=500, blank=True, null=True)
     delivery_days = models.IntegerField(default=0)
     url = models.URLField()
 
