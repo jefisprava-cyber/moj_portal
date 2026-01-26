e9d8dfc8463dc3effbd189516ea4e81153d44c93
jefisprava-cyber <jefisprava@gmail.com>
2026-01-26 21:12:56 +0100

    Fix: Zapnutie WhiteNoise v settings.py

--- core/settings.py
+++ core/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
