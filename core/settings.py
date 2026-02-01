a78e56f17f790ceedb0078611f3b4e12fb087cea
jefisprava-cyber <jefisprava@gmail.com>
2026-02-01 20:40:35 +0100

    Final settings fix for ALLOWED_HOSTS and SSL

--- core/settings.py
+++ core/settings.py
@@ -16,17 +16,28 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-x^7!)5a$1+qia1@w*5d47
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # Na Renderi nastavíme Environment Variable DEBUG = False
-DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+DEBUG = 'RENDER' not in os.environ
 
-# --- DOMÉNY A HOSTING ---
+# --- DOMÉNY A HOSTING (OPRAVENÉ) ---
 
+# Základné domény
 ALLOWED_HOSTS = [
     'jefi.sk',
     'www.jefi.sk',
-    '.onrender.com', # Povolí všetky subdomény na Renderi
     '127.0.0.1',
     'localhost'
 ]
+
+# Pridáme dynamické domény z Renderu (pre istotu)
+render_hosts = os.environ.get('ALLOWED_HOSTS')
+if render_hosts:
+    ALLOWED_HOSTS.extend(render_hosts.split(','))
+else:
+    # Fallback pre subdomény onrender
+    ALLOWED_HOSTS.append('.onrender.com')
+
+# DÔLEŽITÉ PRE RENDER: Aby Django vedelo, že je na HTTPS
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Toto je DÔLEŽITÉ pre formuláre na vlastnej doméne
 CSRF_TRUSTED_ORIGINS = [
@@ -64,7 +75,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [], # Tu môžeš prípadne pridať [BASE_DIR / 'templates'], ak by si mal globálne šablóny
+        'DIRS': [], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -82,7 +93,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        # Lokálne použije sqlite, na Renderi použije PostgreSQL (cez premennú DATABASE_URL)
+        # Lokálne použije sqlite, na Renderi použije PostgreSQL
         default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
         conn_max_age=600
     )
@@ -100,9 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # --- JAZYK A ČAS (Slovensko) ---
 
 LANGUAGE_CODE = 'sk'
-
 TIME_ZONE = 'Europe/Bratislava'
-
 USE_I18N = True
 USE_TZ = True
 
@@ -110,6 +119,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# TOTO JE KĽÚČOVÉ PRE OPRAVU CHYBY 500 (Nemenit na Manifest!):
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # --- AUTENTIFIKÁCIA A PRESMEROVANIA ---
@@ -121,14 +132,12 @@ LOGOUT_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
 
 # --- ODOSIELANIE E-MAILOV (Brevo / SMTP) ---
-# Toto sme pridali, aby fungovalo Resetovanie hesla
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp-relay.brevo.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
-# Tieto hodnoty si ťaháme z Environment Variables na Renderi (bezpečnosť)
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = 'info@jefi.sk'
